30864663a5db0886083300c30fea8f4edd164f24
sheikhshack <redfreak97@gmail.com>
2022-01-21 03:14:00 +0800

    chore: clean up front end template, added route scaffolding (#16)
    * chore: removed travis and gitpod
    
    * chore: added pages for routing
    
    * chore: updated to react-router v6
    
    * feat: route scaffolding

--- frontend/src/app/AppRouter.tsx
+++ frontend/src/app/AppRouter.tsx
@@ -1,28 +1,64 @@
 import { lazy, Suspense } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
-import { LOGIN_ROUTE, ROOT_ROUTE } from '~constants/routes'
+import {
+  BUILDER_ROUTE,
+  DASHBOARD_ROUTE,
+  LOGIN_ROUTE,
+  PUBLIC_ROUTE,
+  VIEWER_ROUTE,
+} from '~constants/routes'
 
 import { PrivateRoute } from './PrivateRoute'
 import { PublicRoute } from './PublicRoute'
 
-const WorkspacePage = lazy(() => import('~features/dashboard/DashboardPage'))
+const DashboardPage = lazy(() => import('~pages/dashboard/DashboardPage'))
+const PublicPage = lazy(() => import('~pages/public/PublicPage'))
+const ViewerPage = lazy(() => import('~pages/viewer/ViewerPage'))
+const BuilderPage = lazy(() => import('~pages/builder/BuilderPage'))
+
 const LoginPage = lazy(() => import('~features/auth/LoginPage'))
+
+// TODO: Add and implement dashboard route (p0)
+// TODO: Add and implement viewer route (p0)
+// TODO: Add and implement builder route (p2)
 
 export const AppRouter = (): JSX.Element => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Switch>
-        <PublicRoute exact path={LOGIN_ROUTE}>
-          <LoginPage />
-        </PublicRoute>
-        <PrivateRoute exact path={ROOT_ROUTE}>
-          <WorkspacePage />
-        </PrivateRoute>
-        <Route path="*">
-          <div>404</div>
-        </Route>
-      </Switch>
+    <Suspense fallback={<div>Loading....</div>}>
+      <Routes>
+        <Route
+          path={LOGIN_ROUTE}
+          element={<PublicRoute strict element={<LoginPage />} />}
+        />
+
+        <Route
+          path={PUBLIC_ROUTE}
+          element={<PublicRoute element={<PublicPage />} />}
+        />
+
+        <Route
+          path={DASHBOARD_ROUTE}
+          element={<PrivateRoute element={<DashboardPage />} />}
+        />
+
+        <Route
+          path={VIEWER_ROUTE}
+          element={<PrivateRoute element={<ViewerPage />} />}
+        />
+
+        <Route
+          path={BUILDER_ROUTE}
+          element={<PrivateRoute element={<BuilderPage />} />}
+        />
+
+        <Route
+          path={LOGIN_ROUTE}
+          element={<PublicRoute strict element={<LoginPage />} />}
+        />
+
+        <Route path="*" element={<div>404 Not Found</div>} />
+      </Routes>
     </Suspense>
   )
 }
---
+++ frontend/src/pages/public/PublicPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface PublicPageProps {}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const PublicPage = ({}: PublicPageProps) => {
+  return <div>I am a public page</div>
+}
+
+export default PublicPage
