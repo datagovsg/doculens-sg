5290f9c08f1ba6d56b70136eb807b017a9232978
sheikhshack <sheikhsalim@mymail.sutd.edu.sg>
2022-01-24 04:43:16 +0800

    chore: move mongo config to config module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common'
-import { SequelizeModule } from '@nestjs/sequelize'
 import { HelmetMiddleware } from 'middlewares/helmet.middleware'
 import { SessionMiddleware } from 'middlewares/session.middleware'
 import { ConfigModule } from 'config/config.module'
@@ -8,11 +7,22 @@ import { OtpModule } from 'otp/otp.module'
 import { MailerModule } from 'mailer/mailer.module'
 import { TerminusModule } from '@nestjs/terminus'
 import { HealthModule } from './health/health.module'
+import { FormsModule } from './forms/forms.module'
+import { MongooseModule } from '@nestjs/mongoose'
+import { SequelizeModule } from '@nestjs/sequelize'
+import { ConfigService } from './config/config.service'
 
 @Module({
   imports: [
     ConfigModule,
     OtpModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        return { uri: configService.get('mongoUrl') }
+      },
+      inject: [ConfigService],
+    }),
     MailerModule,
     SequelizeModule.forRoot({
       dialect: 'sqlite', // TO-DO: change to production database dialect
@@ -22,6 +32,7 @@ import { HealthModule } from './health/health.module'
     AuthModule,
     TerminusModule,
     HealthModule,
+    FormsModule,
   ],
 })
 export class AppModule implements NestModule {
