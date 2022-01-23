580b0884fe65ed60676047cb7ed16906195c53e6
sheikhshack <sheikhsalim@mymail.sutd.edu.sg>
2022-01-23 17:28:50 +0800

    feat: added responses page components

---
+++ frontend/src/pages/responses/components/NestedNavbar.tsx
@@ -0,0 +1,104 @@
+import { FC } from 'react'
+import { BiCog, BiShow } from 'react-icons/all'
+import { useNavigate, useParams } from 'react-router-dom'
+import {
+  Button,
+  HStack,
+  IconButton,
+  Link,
+  Tooltip,
+  useMultiStyleConfig,
+} from '@chakra-ui/react'
+
+import { NavbarBack } from '~components/Navbar/NavbarBack'
+import { NavbarContainer } from '~components/Navbar/NavbarContainer'
+import { NavbarTabs } from '~components/Navbar/NavbarTabs'
+
+const ROUTES = ['responses', 'loader', 'builder']
+
+const NestedNavbar: FC = () => {
+  const navigate = useNavigate()
+  const params = useParams<{ id: string; action: string }>()
+  const navStyles = useMultiStyleConfig('NavbarComponents', {})
+
+  if (!params || !params.id || !params.action) {
+    console.error('invalid route matching detected. Replace with redirect ')
+    throw Error('Bad params error')
+  }
+  const index = ROUTES.indexOf(params.action) || 0
+
+  // Handlers
+
+  const handleReturnBack = () => {
+    navigate(-1)
+  }
+
+  const handleTabChange = (index: number) => {
+    const id = params.id
+    if (id) {
+      navigate({ pathname: `/form/${id}/${ROUTES[index]}` })
+    }
+  }
+
+  const handleOnSettings = () => {
+    console.log('TODO: Handle settings action')
+  }
+
+  const handlePublish = () => {
+    // TODO: Deal with the publishing mechanism
+    console.log('TODO: Handle publish action')
+  }
+
+  return (
+    <>
+      <NavbarContainer
+        leftElement={
+          <NavbarBack label={'placeholder'} handleClick={handleReturnBack} />
+        }
+        centerElement={
+          <NavbarTabs
+            tabTitles={ROUTES}
+            defaultIndex={0}
+            align={'center'}
+            index={index}
+            onChange={handleTabChange}
+          />
+        }
+        rightElement={
+          <HStack>
+            <HStack spacing={0} pr={2}>
+              <IconButton
+                onClick={handleOnSettings}
+                aria-label="Embed or Share"
+                variant="ghost"
+                color="primary.500"
+                icon={<BiCog size="16px" />}
+              />
+              <Link href={`/form/${params.id}/preview`} isExternal>
+                <Tooltip label="Preview">
+                  <IconButton
+                    aria-label="Preview"
+                    variant="ghost"
+                    sx={navStyles.button}
+                    color="primary.500"
+                    icon={<BiShow size="16px" />}
+                  />
+                </Tooltip>
+              </Link>
+            </HStack>
+            <Button
+              variant="solid"
+              sx={navStyles.button}
+              colorScheme="primary"
+              onClick={handlePublish}
+            >
+              Publish changes
+            </Button>
+          </HStack>
+        }
+      />
+    </>
+  )
+}
+
+export default NestedNavbar
