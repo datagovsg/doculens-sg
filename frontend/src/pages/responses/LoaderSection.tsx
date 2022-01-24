596a12713481ea565db1984c906ae2ba9e042211
sheikhshack <sheikhsalim@mymail.sutd.edu.sg>
2022-01-24 14:38:05 +0800

    feat: added loader section

--- frontend/src/pages/responses/LoaderSection.tsx
+++ frontend/src/pages/responses/LoaderSection.tsx
@@ -1,5 +1,23 @@
 import { FC } from 'react'
+import { Container } from '@chakra-ui/react'
+import { Attachment, Toast } from '@opengovsg/design-system-react'
 
 export const LoaderSection: FC = () => {
-  return <div>This is the loader section</div>
+  return (
+    <>
+      <Toast
+        title="This is a BETA feature"
+        description="Have a in-person application? Or your application came through via other mediums? Drag and drop here and we'll take care of it "
+        status="success"
+        onClose={() => null}
+      />
+      <Container pt="50px">
+        <Attachment
+          name="Loader-files"
+          onChange={() => console.log('Drag drop detected')}
+          onError={() => console.error('error detected')}
+        />
+      </Container>
+    </>
+  )
 }
