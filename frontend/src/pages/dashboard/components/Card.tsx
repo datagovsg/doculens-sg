d527727b6e7f99edce301992350606437a02dd2b
sheikhshack <sheikhsalim@mymail.sutd.edu.sg>
2022-01-25 07:29:34 +0800

    chore: updated form tile to follow checkfirst

--- frontend/src/pages/dashboard/components/Card.tsx
+++ frontend/src/pages/dashboard/components/Card.tsx
@@ -1,6 +1,107 @@
-import { FC } from 'react'
+import React, { FC, useRef } from 'react'
+import {
+  BiCog,
+  BiDotsHorizontalRounded,
+  BiDuplicate,
+  BiTrash,
+} from 'react-icons/bi'
 import { Link } from 'react-router-dom'
-import { Text, useMultiStyleConfig, VStack } from '@chakra-ui/react'
+import {
+  Box,
+  CSSObject,
+  Flex,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+  useDisclosure,
+  useMultiStyleConfig,
+  useOutsideClick,
+  VStack,
+} from '@chakra-ui/react'
+
+// Components used only by the card component
+
+interface ActionMenuProps {
+  actions: Array<{
+    label: string
+    onClick: (e: React.MouseEvent) => void
+    icon?: JSX.Element
+    style?: CSSObject
+  }>
+}
+
+const ActionMenu: FC<ActionMenuProps> = ({ actions }) => {
+  const ref = useRef(null)
+  const { isOpen, onClose, onToggle } = useDisclosure()
+  useOutsideClick({
+    ref,
+    handler: () => onClose(),
+  })
+
+  return (
+    <div ref={ref}>
+      <Menu isOpen={isOpen}>
+        <MenuButton
+          as={IconButton}
+          icon={<BiDotsHorizontalRounded />}
+          onClick={(e) => {
+            e.preventDefault()
+            onToggle()
+          }}
+          variant="ghost"
+          border="none"
+        >
+          Click
+        </MenuButton>
+        <MenuList>
+          {actions.map(({ onClick, label, icon, style }, i) => (
+            <MenuItem
+              key={i}
+              icon={icon}
+              sx={style}
+              onClick={(e) => {
+                e.preventDefault()
+                onClose()
+                onClick(e)
+              }}
+            >
+              {label}
+            </MenuItem>
+          ))}
+        </MenuList>
+      </Menu>
+    </div>
+  )
+}
+
+type StatusIndicatorProps = {
+  status: 'active' | 'inactive' | 'draft'
+}
+
+const StatusIndicator: FC<StatusIndicatorProps> = ({ status }) => {
+  const styles = useMultiStyleConfig('Card', {})
+  let color
+  switch (status) {
+    case 'active':
+      color = 'success.500'
+      break
+    case 'inactive':
+      color = 'red.500'
+      break
+    default:
+      color = 'warning.500'
+  }
+
+  return (
+    <Flex sx={styles.indicator} direction="row">
+      <Box h="8px" w="8px" borderRadius="4px" bg={color} mr="8px" />
+      {status}
+    </Flex>
+  )
+}
 
 const Card: FC = () => {
   // TODO: Retrieve all forms properly, remove mocks
@@ -9,6 +110,17 @@ const Card: FC = () => {
   const date = '16 Feb 2021'
 
   const styles = useMultiStyleConfig('Card', {})
+
+  const formActions = [
+    { label: 'Duplicate', icon: <BiDuplicate />, onClick: () => null },
+    { label: 'Settings', icon: <BiCog />, onClick: () => null },
+    {
+      label: 'Delete',
+      icon: <BiTrash />,
+      onClick: () => null,
+      style: { color: 'error.500' },
+    },
+  ]
 
   return (
     <>
@@ -22,6 +134,10 @@ const Card: FC = () => {
               {date}
             </Text>
           </VStack>
+          <Flex direction="row" sx={styles.actions}>
+            <StatusIndicator status={'active'} />
+            <ActionMenu actions={formActions} />
+          </Flex>
         </VStack>
       </Link>
     </>
