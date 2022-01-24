0550e11503bb7254872c859d58adea3d739a0e1b
sheikhshack <sheikhsalim@mymail.sutd.edu.sg>
2022-01-24 14:29:24 +0800

    chore: added sectionalisation of inner navbar

---
+++ frontend/src/pages/responses/LoaderSection.tsx
@@ -0,0 +1,5 @@
+import { FC } from 'react'
+
+export const LoaderSection: FC = () => {
+  return <div>This is the loader section</div>
+}
--- frontend/src/pages/responses/ResponsesPage.tsx
+++ frontend/src/pages/responses/ResponsesPage.tsx
@@ -1,19 +1,36 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import { Container } from '@chakra-ui/react'
 
 import NestedNavbar from '~pages/responses/components/NestedNavbar'
 import ResponseTable from '~pages/responses/components/ResponseTable/ResponseTable'
+import { LoaderSection } from '~pages/responses/LoaderSection'
+import { DashboardSection } from '~pages/responses/types'
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface ResponsesPageProps {}
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const ResponsesPage = ({}: ResponsesPageProps) => {
+  const params = useParams<{ id: string; action: DashboardSection }>()
+
+  const selectedTab = () => {
+    switch (params.action) {
+      case 'loader':
+        return <LoaderSection />
+      case 'responses':
+        return <ResponseTable />
+      default:
+        return <ResponseTable />
+    }
+  }
+
   return (
     <>
+      {/* Implements react-router v5 style for familiarity reasons*/}
       <NestedNavbar />
       <Container maxW="960px" px={0} pt={'100px'}>
-        <ResponseTable />
+        {selectedTab()}
       </Container>
     </>
   )
