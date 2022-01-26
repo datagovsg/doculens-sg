48c8df58bb391508f5cd323c0ad1f919f553a590
loongyh <loongyh@users.noreply.github.com>
2022-01-26 18:24:04 +0800

    feat: add form .json, .md

--- frontend/src/pages/public/components/Hero.tsx
+++ frontend/src/pages/public/components/Hero.tsx
@@ -1,22 +1,31 @@
-import React, { FC } from 'react'
+import React, { FC, useCallback } from 'react'
 import { BiLogOutCircle } from 'react-icons/bi'
 import { Button } from '@opengovsg/design-system-react'
 
 import { useAuth } from '~features/auth'
+import { LOGGED_IN_KEY, useLocalStorage } from '~features/localStorage'
 
 const Hero: FC = () => {
   const { isAuthenticated, email } = useAuth()
 
+  const [, setIsAuthenticated] = useLocalStorage<boolean>(LOGGED_IN_KEY)
+
+  const logout = useCallback(() => {
+    setIsAuthenticated(undefined)
+  }, [setIsAuthenticated])
+
   return (
     <>
-      isAuthenticated &&
-      <Button
-        colorScheme="primary"
-        rightIcon={<BiLogOutCircle fontSize="1.5rem" />}
-        variant="outline"
-      >
-        {email} - Log out
-      </Button>
+      {isAuthenticated && (
+        <Button
+          colorScheme="primary"
+          rightIcon={<BiLogOutCircle fontSize="1.5rem" />}
+          variant="outline"
+          onClick={logout}
+        >
+          {email} - Log out
+        </Button>
+      )}
     </>
   )
 }
