708af3099b51c285b8b75de22f3efb75dc470f54
loongyh <loongyh@users.noreply.github.com>
2022-01-27 03:29:09 +0800

    fix: list-style-types for lists

--- frontend/src/pages/public/components/Instructions.tsx
+++ frontend/src/pages/public/components/Instructions.tsx
@@ -1,6 +1,6 @@
 import { FC, ReactNode, useEffect, useState } from 'react'
-import ReactMarkdown from 'react-markdown'
-import { Link, Text } from '@chakra-ui/react'
+import ReactMarkdown, { Components } from 'react-markdown'
+import { Link, OrderedList, Text, UnorderedList } from '@chakra-ui/react'
 import ChakraUIRenderer from 'chakra-ui-markdown-renderer'
 
 import md from '../instructions.md'
@@ -18,17 +18,54 @@ export const Instructions: FC = () => {
       </Text>
     )
   }
-  const link: FC = (props) => {
-    return <Link {...props} isExternal />
-  }
-  const theme = {
+  const theme: Components = {
     h1: heading,
     h2: heading,
     h3: heading,
     h4: heading,
     h5: heading,
     h6: heading,
-    a: link,
+    a: (props) => <Link {...props} isExternal />,
+    ol: (props) => {
+      const { ordered, children, depth } = props
+      const Element = ordered ? OrderedList : UnorderedList
+      const styleType = {
+        0: ordered ? 'decimal' : 'disc',
+        1: ordered ? 'lower-roman' : 'circle',
+        2: ordered ? 'lower-alpha' : 'square',
+      }[depth % 3]
+      return (
+        <Element
+          spacing={2}
+          as={ordered ? 'ol' : 'ul'}
+          styleType={styleType}
+          pl={4}
+          {...props}
+        >
+          {children}
+        </Element>
+      )
+    },
+    ul: (props) => {
+      const { ordered, children, depth } = props
+      const Element = ordered ? OrderedList : UnorderedList
+      const styleType = {
+        0: ordered ? 'decimal' : 'disc',
+        1: ordered ? 'lower-roman' : 'circle',
+        2: ordered ? 'lower-alpha' : 'square',
+      }[depth % 3]
+      return (
+        <Element
+          spacing={2}
+          as={ordered ? 'ol' : 'ul'}
+          styleType={styleType}
+          pl={4}
+          {...props}
+        >
+          {children}
+        </Element>
+      )
+    },
   }
 
   useEffect(() => {
