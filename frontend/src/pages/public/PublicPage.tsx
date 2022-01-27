cdd22c817289f5b806462a5269a807aeb100081a
loongyh <loongyh@users.noreply.github.com>
2022-01-27 16:16:00 +0800

    feat: add conditional fields

--- frontend/src/pages/public/PublicPage.tsx
+++ frontend/src/pages/public/PublicPage.tsx
@@ -3,7 +3,7 @@ import { SubmitHandler, useForm } from 'react-hook-form'
 import { VStack } from '@chakra-ui/react'
 
 //import { GovtMasthead } from '@opengovsg/design-system-react' // Pending design-system-react 0.0.6 release
-import { Hero, Instructions, Section } from './components'
+import { Hero, Instructions, Section, SectionData } from './components'
 import { body, estmins, title } from './form.json'
 
 export interface FormValues {
@@ -25,6 +25,7 @@ const PublicPage: FC = () => {
     formState: { errors },
   } = useForm<FormValues>()
   const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data)
+  const watchShowAge = watch('Unemployed', 0) // you can supply default value as second argument
 
   return (
     <>
@@ -33,7 +34,7 @@ const PublicPage: FC = () => {
       <VStack py="84px" spacing={6}>
         <Instructions />
         <form onSubmit={handleSubmit(onSubmit)}>
-          {body.map((section, index) => {
+          {(body as SectionData[]).map((section, index) => {
             return (
               <Section
                 key={index}
